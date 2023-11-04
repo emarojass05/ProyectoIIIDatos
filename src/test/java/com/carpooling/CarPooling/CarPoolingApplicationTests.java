ff8efe02b745d40caf0876aaaf7a22aa709a8af0
SpaceBa13 <brayanalpizar36@gmail.com>
2023-11-03 19:55:24 -0600

    Maven and Spring boot

---
+++ src/test/java/com/carpooling/CarPooling/CarPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpooling.CarPooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
