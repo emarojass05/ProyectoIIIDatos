68822181382dd4019eafeea4062fde0e2b6c2db9
EMERSON_MONGE_H <mongehernandezemerson@gmail.com>
2023-11-16 01:16:09 -0600

    [Add] Mapa cartago y algoritmo base de prueba restapi

--- Clients/app/src/main/java/com/example/login/MainActivity.java
+++ Clients/app/src/main/java/com/example/login/MainActivity.java
@@ -6,11 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mLogin;
     private Button mRegister;
+    private TextView checkCred;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 return;
+            }
+        });
+
+        mLogin = (Button) findViewById(R.id.btnLogin);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+                /*
+                try {
+                    URL url = new URL("localhost:8080/login");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer content = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    con.disconnect();
+
+                    System.out.println(content.toString());
+
+                } catch (Exception e) {
+                    checkCred = (TextView) findViewById(R.id.CheckCredText);
+                    checkCred.setVisibility(View.VISIBLE);
+                }
+
+                 */
             }
         });
 
